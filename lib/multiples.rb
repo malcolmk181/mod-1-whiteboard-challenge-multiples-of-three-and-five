0bcbc083d59df4f4e5074a6ab7ddddf94f8de2e4
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-01 15:55:55 -0400

    commit tree ac217711471855bf26143c8cfe7dcaca4b3d6e6c

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    (1..(limit - 1)).to_a.filter { |num| num % 3 == 0 || num % 5 == 0 }
+end
+
+def sum_multiples(limit)
+    sum = 0
+    collect_multiples(limit).each { |num| sum += num}
+    sum
+end

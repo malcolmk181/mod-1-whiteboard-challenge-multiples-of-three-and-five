253b1c1202d9a1e4034592a7274e3cc36694461c
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-01 15:58:42 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1..(@limit - 1)).to_a.filter { |num| num % 3 == 0 || num % 5 == 0 }
+    end
+    
+    def sum_multiples
+        sum = 0
+        collect_multiples.each { |num| sum += num}
+        sum
+    end
+end
